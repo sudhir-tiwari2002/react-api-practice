b8004436ac77b07fba33479f6e6ad3a1f90e5dee
Sudhir Tiwari <skt2ktiwari@gmail.com>
2023-09-10 16:56:21 +0530

    refetch on mount and refetch on focus

--- src/components/Rq-super-heroes.jsx
+++ src/components/Rq-super-heroes.jsx
@@ -11,11 +11,15 @@ function RqSuperHeroes() {
     "super-heroes",
     fetchSuperHeros,
     {
-      // cacheTime: 6000,       // Bydefault cachetime of a query is 5 mint after 5 mint query will be garbage collected . but we can initilize the catchtime by doing this
-      staleTime: 30000,                      // query cache helps us to prevent the unneccessary newtwork call by the setting stale time query will fresh till the stale time over and it will prevent to network call. default stale time is 0 second because many application need to fetch fresh data on every second 
-    
-    }                   
-  );
+      // cacheTime: 6000,                // Bydefault cachetime of a query is 5 mint after 5 mint query will be garbage collected . but we can initilize the catchtime by doing this
+      // staleTime: 30000,              // query cache helps us to prevent the unneccessary newtwork call by the setting stale time query will fresh till the stale time over and it will prevent to network call. default stale time is 0 second because many application need to fetch fresh data on every second 
+        refetchOnMount : 'always',     // by default it is set to true if it is set to true query will refech on mount if the data is stale , data will fetch every time component mount
+                                      // by setting the refetchOnmount always in the string the refeching will be done irreseptive on the stale , true is the best option 
+      refetchOnWindowFocus:true,     // bydefault it is set to true , everytime when user open the particular ui tab refech will perform
+  
+  
+    }
+         );
 
   // const {isLoading,data} =useQuery('super-heroes',()=>{ return axios.get('http://localhost:3000/superheroes')})
 
