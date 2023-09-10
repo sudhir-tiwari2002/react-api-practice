090fce1c1eb66c1b306f1d59a08287bab1538f75
Sudhir Tiwari <skt2ktiwari@gmail.com>
2023-09-10 20:52:47 +0530

    second way to send the dynamic id

--- src/hooks/useSuperHeroDataById.jsx
+++ src/hooks/useSuperHeroDataById.jsx
@@ -1,11 +1,25 @@
 import {useQuery} from 'react-query'
 import axios from "axios";
 
-const fetchSuperHero = heroId =>{
+// const fetchSuperHero = heroId =>{
+//     return axios.get(`http://localhost:3000/superheroes/${heroId}`)
+// }
+
+
+// export const useSuperHeroDataById = (heroId) =>{
+//     console.log(heroId,".......")
+//     return useQuery(['super-hero',heroId],()=>fetchSuperHero(heroId))
+// }
+
+
+const fetchSuperHero = ({queryKey}) =>{
+    const heroId = queryKey[1]
     return axios.get(`http://localhost:3000/superheroes/${heroId}`)
 }
 
+
+
 export const useSuperHeroDataById = (heroId) =>{
     console.log(heroId,".......")
-    return useQuery(['super-hero',heroId],()=>fetchSuperHero(heroId))
+    return useQuery(['super-hero',heroId],fetchSuperHero)
 }
