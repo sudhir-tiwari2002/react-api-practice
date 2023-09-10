63491845322ce75695f2a9ef4600ab4d9e9947e7
Sudhir Tiwari <skt2ktiwari@gmail.com>
2023-09-10 16:35:17 +0530

    cachetime setup

--- src/components/Rq-super-heroes.jsx
+++ src/components/Rq-super-heroes.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import axios from 'axios';
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
-const fetchSuperHeros = () =>{
-  return axios.get('http://localhost:3000/superheroes')
-}
+const fetchSuperHeros = () => {
+  return axios.get("http://localhost:3000/superheroes");
+};
 
 function RqSuperHeroes() {
-  const {isLoading,data , isError ,error} =useQuery('super-heroes',fetchSuperHeros)
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    fetchSuperHeros,
+    {
+      cacheTime: 6000, // Bydefault cachetime of a query is 5 mint after 5 mint query will be garbage collected . but we can initilize the catchtime by doing this
+    }
+  );
 
   // const {isLoading,data} =useQuery('super-heroes',()=>{ return axios.get('http://localhost:3000/superheroes')})
 
-  if(isLoading){
-    return (<h1>Loading...</h1>)
+  if (isLoading) {
+    return <h1>Loading...</h1>;
   }
-  if(isError){
-    return (<h1> {error.message}</h1>)
+  if (isError) {
+    return <h1> {error.message}</h1>;
   }
-  
+
   return (
     <>
       <h1> welcome from the pages of RQ super Heros</h1>
-      {
-        data?.data?.map((item)=>(
-          <div key={item?.id}>
-            <p>ID of Super hero : {item?.id}</p>
-            <p>Name of the Super Hero : {item?.name}</p>
-            <p>AlterEgo of the Super Hero : {item?.alterEgo}</p>
-
-          </div>
-        ))
-      }
-      </>
+      {data?.data?.map((item) => (
+        <div key={item?.id}>
+          <p>ID of Super hero : {item?.id}</p>
+          <p>Name of the Super Hero : {item?.name}</p>
+          <p>AlterEgo of the Super Hero : {item?.alterEgo}</p>
+        </div>
+      ))}
+    </>
   );
 }
 
