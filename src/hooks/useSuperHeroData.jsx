36815ce20c8d2680b53e921f534988ad45411b0d
Sudhir Tiwari <skt2ktiwari@gmail.com>
2023-09-10 19:48:24 +0530

    creating custom hook to fetch data

---
+++ src/hooks/useSuperHeroData.jsx
@@ -0,0 +1,23 @@
+import {useQuery} from 'react-query'
+import axios from "axios";
+
+const fetchSuperHeros = () => {
+    return axios.get("http://localhost:3000/superheroes");
+  };
+
+
+export const useSuperHeroData = (onSuccess,onError) =>{
+    return useQuery(
+        "super-heroes",
+        fetchSuperHeros,
+        {  
+        onSuccess,
+        onError,
+         select:(data)=>{
+            const superHeroNames = data?.data?.map(hero => hero.name)
+            return superHeroNames
+          },
+        }
+             );
+}
+
