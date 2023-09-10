5051b2c5ad5622f4d1fd7a039814946d29c0c054
Sudhir Tiwari <skt2ktiwari@gmail.com>
2023-09-10 18:52:24 +0530

    onsuccess and onerror callback handle

--- src/components/Rq-super-heroes.jsx
+++ src/components/Rq-super-heroes.jsx
@@ -7,6 +7,14 @@ const fetchSuperHeros = () => {
 };
 
 function RqSuperHeroes() {
+
+  const onSuccess = (data) =>{
+    console.log("perform side effect after data fetching",data)
+  }
+  const onError = (error) =>{
+    console.log("perform side effect after encoutering error",error)
+  }
+
   const { isLoading, data, isError, error , refetch} = useQuery(
     "super-heroes",
     fetchSuperHeros,
@@ -18,11 +26,12 @@ function RqSuperHeroes() {
       // refetchOnWindowFocus:true,     // bydefault it is set to true , everytime when user open the particular ui tab refech will perform
       // refetchInterval: 2000,         // polling refers to feching data on the regular intervals , by default it is set to false if we set any time the query will refetch data on that particular time stamp . In our case i am setting the refechinterval 2 second that means after every 2 second data will refech
       // refetchIntervalInBackground:true  // it will usefull to refetch data even when the brouser is not in focus (ON)
-      enabled:false,                 // suppose i don't need to fetch data on the component mount the by the setting enable false newtwork call will not triggered [ if i want to trigger network request on the click of a button then we use refetch method of api call]
+      // enabled:false,                 // suppose i don't need to fetch data on the component mount the by the setting enable false newtwork call will not triggered [ if i want to trigger network request on the click of a button then we use refetch method of api call]
     
-    
-    
-    
+    // lets learn about callbacks using react query we are dealing with data fatching sometimes you might want to perform some side- effect when the quwery complete and example to open modal or navigating routes etc
+    // react query automatically enjects the data and error into these callbacks 
+    onSuccess: onSuccess,
+    onError:onError
     
     }
          );
